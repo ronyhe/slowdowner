ff6f90d9b21f66bb8396ffa69567ad435d4c1be1
ronyhe <ronyheruth@wix.com>
2022-08-03 16:01:11 +0300

    Some styling

--- src/Slowdowner.tsx
+++ src/Slowdowner.tsx
@@ -1,15 +1,18 @@
 import FileUploader from './FileUploader'
+import Bar from './Bar'
+import { ZERO } from './time'
 
 function Slowdowner() {
     return (
-        <>
+        <div style={{ maxWidth: '400px', maxHeight: '150px', margin: '20px' }}>
             <FileUploader
                 file={null}
                 onFileChosen={(file: File) => {
                     console.log(file)
                 }}
             />
-        </>
+            <Bar min={ZERO} max={{ minutes: 3, seconds: 0 }} current={ZERO} />
+        </div>
     )
 }
 

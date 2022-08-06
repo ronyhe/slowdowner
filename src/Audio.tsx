d421d08e18ba894affe6d47f30eda9aa397d6f21
ronyhe <ronyheruth@wix.com>
2022-08-06 18:39:05 +0300

    Hooks

--- src/Audio.tsx
+++ src/Audio.tsx
@@ -1,22 +1,44 @@
-import React, { useEffect, useRef } from 'react'
+import React, { DependencyList, useEffect, useRef } from 'react'
+
+function useAudioEffect(
+    audioRef: React.RefObject<HTMLAudioElement>,
+    func: (audio: HTMLAudioElement) => void,
+    deps?: DependencyList
+) {
+    useEffect(() => {
+        if (audioRef.current) {
+            func(audioRef.current)
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [audioRef.current, ...(deps ?? [])])
+}
+
+function useAudioEffects(audioRef: React.RefObject<HTMLAudioElement>) {
+    return (func: (audio: HTMLAudioElement) => void, deps?: DependencyList) => {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        useAudioEffect(audioRef, func, deps)
+    }
+}
 
 function Audio(props: { file: File; shouldPlay: boolean }) {
     const ref = useRef<HTMLAudioElement>(null)
-    const audio = ref.current
-    useEffect(() => {
-        if (audio) {
+    const useAudio = useAudioEffects(ref)
+    useAudio(
+        audio => {
             audio.src = URL.createObjectURL(props.file)
-        }
-    }, [audio, props.file])
-    useEffect(() => {
-        if (audio) {
+        },
+        [props.file]
+    )
+    useAudio(
+        audio => {
             if (props.shouldPlay) {
                 audio.play()
             } else {
                 audio.pause()
             }
-        }
-    }, [audio, props.shouldPlay])
+        },
+        [props.shouldPlay]
+    )
     return (
         <>
             <audio ref={ref} />

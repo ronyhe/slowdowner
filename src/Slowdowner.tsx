317debfa39ab4d09ca9ec42c1a118fa1e710c5f1
ronyhe <ronyheruth@wix.com>
2022-08-07 23:18:54 +0300

    Paper

--- src/Slowdowner.tsx
+++ src/Slowdowner.tsx
@@ -5,6 +5,8 @@ import PlayPauseButton, { PlayPauseStatus } from './PlayPauseButton'
 import React, { useState } from 'react'
 import SpeedController from './SpeedController'
 import Audio from './Audio'
+import Paper from '@mui/material/Paper'
+import Box from '@mui/material/Box'
 
 const THREE_MINUTES = { minutes: 3, seconds: 0 }
 
@@ -37,28 +39,30 @@ function Slowdowner() {
         />
     ) : null
     return (
-        <div style={{ maxWidth: '400px', maxHeight: '150px', margin: '20px' }}>
-            <FileUploader
-                file={file}
-                onFileChosen={file => {
-                    setFile(file)
-                    setPlayPauseStatus('paused')
-                }}
-            />
-            <Bar
-                onStartChange={setStartTime}
-                onEndChange={setEndTime}
-                max={duration}
-                current={currentTime}
-                start={startTime}
-                end={endTime}
-            />
-            <PlayPauseButton
-                status={playPauseStatus}
-                onStatusChange={setPlayPauseStatus}
-            />
-            <SpeedController speed={speed} onSpeedChange={setSpeed} />
-            {audio}
+        <div style={{ maxWidth: '500px', maxHeight: '150px', margin: '20px' }}>
+            <Paper elevation={2} sx={{ padding: '20px' }}>
+                <FileUploader
+                    file={file}
+                    onFileChosen={file => {
+                        setFile(file)
+                        setPlayPauseStatus('paused')
+                    }}
+                />
+                <Bar
+                    onStartChange={setStartTime}
+                    onEndChange={setEndTime}
+                    max={duration}
+                    current={currentTime}
+                    start={startTime}
+                    end={endTime}
+                />
+                <PlayPauseButton
+                    status={playPauseStatus}
+                    onStatusChange={setPlayPauseStatus}
+                />
+                <SpeedController speed={speed} onSpeedChange={setSpeed} />
+                {audio}
+            </Paper>
         </div>
     )
 }

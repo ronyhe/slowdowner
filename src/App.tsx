089cffa21f59fd9b9188557854416ae14402a63d
ronyhe <ronyheruth@wix.com>
2022-08-07 23:20:56 +0300

    css baseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
 import React from 'react'
-// import logo from './logo.svg'
-// import './App.css'
 import Slowdowner from './Slowdowner'
+import CssBaseline from '@mui/material/CssBaseline'
 
 function App() {
     return (
-        <Slowdowner />
-        // <div className='App'>
-        //     <header className='App-header'>
-        //         <img src={logo} className='App-logo' alt='logo' />
-        //         <p>
-        //             Edit <code>src/App.tsx</code> and save to reload.
-        //         </p>
-        //         <a
-        //             className='App-link'
-        //             href='https://reactjs.org'
-        //             target='_blank'
-        //             rel='noopener noreferrer'
-        //         >
-        //             Learn React
-        //         </a>
-        //     </header>
-        // </div>
+        <CssBaseline>
+            <Slowdowner />
+        </CssBaseline>
     )
 }
 

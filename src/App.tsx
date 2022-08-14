b81d7681b7205ea794fb9cd85dc2c44839f8743b
ronyhe <ronyheruth@wix.com>
2022-08-14 07:07:34 +0300

    Switch theme colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Slowdowner from './Slowdowner'
 import CssBaseline from '@mui/material/CssBaseline'
+import { createTheme, ThemeProvider } from '@mui/material'
+import {} from '@mui/system/useTheme'
+
+const theme = createTheme()
+const { primary, secondary } = theme.palette
+theme.palette.primary = secondary
+theme.palette.secondary = primary
 
 function App() {
     return (
         <CssBaseline>
-            <Slowdowner />
+            <ThemeProvider theme={theme}>
+                <Slowdowner />
+            </ThemeProvider>
         </CssBaseline>
     )
 }

6c3c49c83c910f6c6dbf1b95734ca0468342d6c8
ronyhe <ronyheruth@wix.com>
2022-08-14 21:08:00 +0300

    Remove state

--- src/SpeedController.tsx
+++ src/SpeedController.tsx
@@ -1,11 +1,10 @@
 import TextField from '@mui/material/TextField'
-import React, { useState } from 'react'
+import React from 'react'
 
 function SpeedController(props: {
     speed: number
     onSpeedChange: (speed: number) => void
 }) {
-    const [speed, setSpeed] = useState(props.speed)
     return (
         <>
             <TextField
@@ -13,14 +12,13 @@ function SpeedController(props: {
                 variant='filled'
                 label='speed'
                 type='number'
-                value={speed.toFixed(1)}
+                value={props.speed.toFixed(1)}
                 inputProps={{
                     step: 0.1
                 }}
                 onChange={e => {
                     const newSpeed = parseFloat(e.target.value)
                     props.onSpeedChange(newSpeed)
-                    setSpeed(newSpeed)
                 }}
             />
         </>
